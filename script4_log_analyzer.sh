73a08e450b29957816f66371b86c53846c3423b2
RajatVerma24bsa10370 <rajat.24bsa10370@vitbhopal.ac.in>
2026-03-28 21:54:27 +0530

    Update script4_log_analyzer.sh

---
+++ script4_log_analyzer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+LOGFILE=$1
+KEYWORD=${2:-"error"}
+COUNT=0
+
+while IFS= read -r LINE; do
+    if echo "$LINE" | grep -iq "$KEYWORD"; then
+        COUNT=$((COUNT + 1))
+    fi
+done < "$LOGFILE"
+
+echo "Keyword '$KEYWORD' found $COUNT times"
